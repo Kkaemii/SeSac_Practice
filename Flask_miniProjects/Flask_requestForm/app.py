6437c1a7ed0801a5dcd362b5274789a30517cd33
Kkaemii <hhhmh0530@gmail.com>
2025-02-04 19:19:57 +0900

    email_validator, flash 적용중
    index 및 conatact_complete html에 flash 적용시키기 (jinja2)
    email_validator 적용하기

--- Flask_miniProjects/Flask_requestForm/app.py
+++ Flask_miniProjects/Flask_requestForm/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for, redirect,request, session
+from email_validator import validate_email, EmailNotValidError
+from flask import Flask, render_template, url_for, redirect,request, session, flash
+
 app = Flask(__name__)
 app.secret_key = '움하하하하하하!'
 
@@ -16,13 +18,41 @@ def contact():
 @app.route('/contact_save', methods=['POST'])
 def contact_save():
   description = request.form.get('description','')
+  username = request.form.get('username', '')
+  email = request.form.get('email','')
   session['description'] = description #세션에 description 저장
+  session['username'] = username
+  session['email'] = email
+
+  #사용자의 입력 체크
+  is_valid = True
+  if not username:
+    flash('사용자 명은 필수입니다.')
+    is_valid = False
+  
+  if not email:
+    flash('메일 주소는 필수입니다.')
+    is_valid = False
+    try:
+      validate_email(email)
+    except EmailNotValidError:
+      flash('메일 주소 형식으로 입력해주세요.')
+    is_valid = False
+
+  if not description:
+    flash('문의 내용은 필수입니다.')
+    is_valid=False
+
+  if not is_valid:
+    return redirect(url_for('contact'))
+  
+  flash('문의내용이 정상적으로 전달되었습니다. 문의해주셔서 감사합니다.')
   return redirect(url_for('complete' ))
 
 
-@app.route('/complete', methods=['POST','GET'])
+@app.route('/complete', methods=['POST', 'GET'])
 def complete():
-  if not session.get('description'):
+  if not session.get('description') or not session.get('username') or not session.get('email'):
     return redirect('contact')
   description = session.get('description', '') #세션에 저장한 값 가지고 오기
 
